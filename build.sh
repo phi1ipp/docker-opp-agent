559fcf264d2c12f68299d88600a5bd2c3498b9ed
Philipp Grigoryev <philipp.grigoryev@icsynergy.com>
2020-03-19 16:20:19 -0500

    Update build.sh
    comments for better understanding of the process

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
+###### building registration image #######
 cd registration
 docker build --build-arg domain=$1 --build-arg agent_version=$2 -t phi1ipp/opp-agent .
+
+###### starting container #######
 docker run -d --rm --name=agent phi1ipp/opp-agent
 
+###### saving binaries from registration container #######
 docker cp agent:/opt/OktaProvisioningAgent ../
 docker rm -f agent
 
+##### building runtime image #####
 cd ../runtime
 docker build -t opp-runtime .
 
-####### optional, you can run it from your location ########
+####### here we start a runtime container with an external volume ########
 cd ..
 docker run --rm -v $(pwd)/OktaProvisioningAgent:/opt/OktaProvisioningAgent agent-runtime
